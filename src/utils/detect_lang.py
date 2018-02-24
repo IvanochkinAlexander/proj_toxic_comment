de66da1f16d0100bb7916254178eee46105c9bd2
IvanochkinAlexander <kasperrrok@gmail.com>
2018-02-24 13:07:39 +0100

    [+] changed preprocessing part

--- src/utils/detect_lang.py
+++ src/utils/detect_lang.py
@@ -25,10 +25,32 @@ def detect_lang(col):
         return 'Unkmown'
 
 ### executing script
-train_test=pd.read_json('../input/toxic/train_test.json')
-train_test = train_test.rename(columns={'0':0})
+train_test=pd.read_json('../../data/interim/train_test_corrected.json')
+# train_test = train_test.rename(columns={'0':0})
 train_test = train_test.reset_index(drop=True)
 # train_test= train_test[:100]
-train_test['lang'] =train_test[0].apply(detect_lang)
-send_to_telegram('язык готов')
-train_test.to_csv('../output/train_test_lang.csv')
+train_test['lang'] =train_test['comment_text_corrected'].apply(detect_lang)
+
+def split_col_1 (col):
+    try:
+        col1 = col[0]
+        return col1
+    except:
+        return 'unknown'
+
+train_test['lang_1'] = train_test['lang'].apply(split_col_1)
+
+def split_col_2 (col):
+    try:
+        col1 = col[1]
+        return col1
+    except:
+        return 'unknown'
+
+train_test['lang_2'] = train_test['lang'].apply(split_col_2)
+train_test['one_lang'] = (train_test['lang_1']==train_test['lang_2']).astype(int)
+
+
+
+# send_to_telegram('язык готов')
+train_test.to_json('../../data/interim/train_test_lang.json')
---
+++ src/utils/run_preproc.py
@@ -0,0 +1,51 @@
+import subprocess
+import os
+import telegram
+import time
+import pandas as pd
+
+def send_to_telegram(text):
+
+    """Send appropriate links to telegram channel"""
+
+    bot = telegram.Bot(token='523412387:AAHhEckKtZiCoSG6Pd3ZGtp4-JbL06I8H2E')
+    # chat_id = -1001371737931
+    chat_id = 169719023
+    bot.send_message(chat_id=chat_id, text=text)
+    time.sleep(5)
+
+process = subprocess.Popen("python convert_non_ascii.py",shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+out, err = process.communicate()
+errcode = process.returncode
+
+send_to_telegram('converted to ascii')
+time.sleep(20)
+
+
+process = subprocess.Popen("python preproc_replace.py",shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+out, err = process.communicate()
+errcode = process.returncode
+
+send_to_telegram('replaced wrong text')
+time.sleep(20)
+
+python3_command = "python2.7 spell_checker.py"  # launch your python2 script using bash
+process = subprocess.Popen(python3_command.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()  # receive output from the python2 script
+
+send_to_telegram('spell checking finished')
+time.sleep(20)
+
+process = subprocess.Popen("python detect_lang.py",shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+out, err = process.communicate()
+errcode = process.returncode
+
+send_to_telegram('languages detected')
+time.sleep(20)
+
+process = subprocess.Popen("python preproc_lemm.py",shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+out, err = process.communicate()
+errcode = process.returncode
+
+send_to_telegram('lemmatization with spacy finished')
+time.sleep(20)
